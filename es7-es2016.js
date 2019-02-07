857d760a15582e950c65444126cb882adb699bdd
Eagan Martin <mece.martinece@gmail.com>
2019-02-07 11:23:05 +0800

    Added guide for es2016

---
+++ es7-es2016.js
@@ -0,0 +1,11 @@
+/**
+ * ES7 (ES2016)
+ */
+
+/** Array Prototype Includes */
+const anotherArray = ['another', 'array']
+anotherArray.includes('another')
+
+/** Exponential Operator */
+const anotherSquareArea = Math.pow(50, 2)
+const justAnotherSquareArea = 50 ** 2
